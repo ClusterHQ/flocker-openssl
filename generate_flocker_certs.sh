22616e1ad426bb24658d0f628a79f92f31fad210
Srdjan Grubor <sgnn7@sgnn7.org>
2016-03-31 11:26:57 -0500

    Simplified function usage even more
    Simpler & more streamlined but it could still use a bit more cleanup.

--- generate_flocker_certs.sh
+++ generate_flocker_certs.sh
@@ -144,15 +144,15 @@ generate_csr() {
 }
 
 sign_csr() {
-  # sign_csr <csr_path> <ca_key> <ca_crt> <output_path> [<extension>]
+  # sign_csr <csr_path> <ca_keypair> <output_path> [<extension>]
   local csr_path="${1}"
-  local ca_key="${2}"
-  local ca_crt="${3}"
-  local output_path="${4}"
+  local ca_key="${2}.key"
+  local ca_crt="${2}.crt"
+  local output_path="${3}"
 
   local extensions=""
-  if [ $# -gt 4 ] && [ ! -z "${5}" ]; then
-    extensions="-extensions ${5}"
+  if [ $# -gt 3 ] && [ ! -z "${4}" ]; then
+    extensions="-extensions ${4}"
   fi
 
   openssl ca -batch \
@@ -168,21 +168,19 @@ sign_csr() {
 }
 
 generate_and_sign_cert() {
-  # generate_and_sign_cert <key_path> <subject> <cluster_crt> <cluster_key> <crt_output> [<extensions>]
-  local key_path="${1}"
+  # generate_and_sign_cert <keypair_path> <subject> <ca_keypair> [<extensions>]
+  local key_path="${1}.key"
+  local crt_output="${1}.crt"
   local subject="${2}"
-  local cluster_crt="${3}"
-  local cluster_key="${4}"
-  local crt_output="${5}"
-  local extensions="${6}"
+  local ca_keypair="${3}"
+  local extensions="${4}"
 
   local temp_csr_path=$(mktemp -q "$(basename $0).XXXXX")
 
   generate_key "$key_path"
   generate_csr "$key_path" "$subject" "$temp_csr_path"
   sign_csr "$temp_csr_path" \
-         "$cluster_key" \
-         "$cluster_crt" \
+         "$ca_keypair" \
          "$crt_output" \
          "$extensions"
 
@@ -192,9 +190,10 @@ generate_and_sign_cert() {
 
 echo "Generating the CA keypair"
 cluster_uuid=$(uuidgen)
-cluster_key_path=cluster.key
-cluster_crt_path=cluster.crt
-subject="/CN=$cluster_name/OU=$cluster_uuid"
+cluster_keypair_path=cluster
+cluster_key_path="${cluster_keypair_path}.key"
+cluster_crt_path="${cluster_keypair_path}.crt"
+
 generate_key $cluster_key_path
 openssl req -batch \
             -config $openssl_cnf_path \
@@ -204,46 +203,32 @@ openssl req -batch \
             -days 7300 \
             -sha256 \
             -extensions v3_ca \
-            -subj "$subject" \
+            -subj "/CN=$cluster_name/OU=$cluster_uuid" \
             -out $cluster_crt_path
 
 echo "Generating the control service keypair"
 # These end up getting copied to the nodes as control-service.(key|crt)
-control_key_path=control-$control_host.key
-control_crt_path=control-$control_host.crt
-subject="/CN=control-service/OU=$cluster_uuid"
-generate_and_sign_cert "$control_key_path" \
-                       "$subject" \
-                       "$cluster_crt_path" \
-                       "$cluster_key_path" \
-                       "$control_crt_path" \
+control_keypair_path=control-$control_host
+generate_and_sign_cert "$control_keypair_path" \
+                       "/CN=control-service/OU=$cluster_uuid" \
+                       "$cluster_keypair_path" \
                        "control_service_extension"
 
 echo "Generating node keypair(s)"
 for node_hostname in ${nodes[@]}; do
     mkdir -p $node_hostname
 
-    node_uuid=$(uuidgen)
-    node_key_path=$node_hostname/node-$node_uuid.key
-    node_crt_path=$node_hostname/node-$node_uuid.crt
-
-    subject="/CN=node-$node_uuid/OU=$cluster_uuid"
-    generate_and_sign_cert "$node_key_path" \
-                           "$subject" \
-                           "$cluster_crt_path" \
-                           "$cluster_key_path" \
-                           "$node_crt_path"
+    node_keypair_path=$node_hostname/node-$node_uuid
+    generate_and_sign_cert "$node_keypair_path" \
+                           "/CN=node-$(uuidgen)/OU=$cluster_uuid" \
+                           "$cluster_keypair_path"
 done
 
 echo "Generating API keypair"
 api_username=api_user
-api_key_path=$api_username.key
-api_crt_path=$api_username.crt
-subject="/CN=user-$api_username/OU=$cluster_uuid"
-generate_and_sign_cert "$api_key_path" \
-                       "$subject" \
-                       "$cluster_crt_path" \
-                       "$cluster_key_path" \
-                       "$api_crt_path" \
+api_keypair_path=$api_username
+generate_and_sign_cert "$api_keypair_path" \
+                       "/CN=user-$api_username/OU=$cluster_uuid" \
+                       "$cluster_keypair_path" \
                        "client_api_ext"
 echo "Done!"
