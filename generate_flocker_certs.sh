fb3c46c64f36b54a90f92de9107c2e5f298aefac
Srdjan Grubor <sgnn7@sgnn7.org>
2016-03-31 10:24:49 -0500

    Cleaned up the script a bit
    Code is now easier to read and maintain, we have key-length variable and
    a bit cleaner way to create the certs.

--- generate_flocker_certs.sh
+++ generate_flocker_certs.sh
@@ -1,28 +1,18 @@
 #!/bin/bash -e
-
-IFS=","
-
-# README:
-# the first time you run this, you'll need to setup your
-# certificate store (feel free to create these example directories
-# whereever you feel is appropriate):
 #
-# mkdir $HOME/ssl
-# cd $HOME/ssl
-# mkdir csr newcerts
-# touch index.txt
-# echo 1000 > serial
-#
-# you will also need to update the CA_default section of openssl.cnf
-# to point at your ssl directory.
-#
-# most of this was taken from this tutorial:
+################################################################################
+# Most of this was taken from this tutorial:
 # https://jamielinux.com/docs/openssl-certificate-authority/index.html
 # and combined with the implementation in flocker/flocker/ca/_ca.py
 #
-# authors: Brendan Cox "justnoise" , and Ryan Wallner "wallnerryan"
+# Authors:
+# - Brendan Cox "justnoise"
+# - Ryan Wallner "wallnerryan"
+# - Srdjan Grubor <sgnn7@sgnn7.org>
 #
 ################################################################################
+
+CURRENT_DIR="$(dirname $0)"
 
 HELP_MSG="""
 Usage: $0 (-i=<control_ip> | -d=<control_fqdn>) [-f=openssl_conf] -c=<cluster_name> -n=<node>[,<node> ... ]
@@ -33,6 +23,7 @@ Usage: $0 (-i=<control_ip> | -d=<control_fqdn>) [-f=openssl_conf] -c=<cluster_na
 
 # Optional
 -c= | --cluster_name= (Name of your cluster, should be unique. Default=mycluster)
+-k= | --key_size= (Size of RSA keys - defaults to 4096)
 -f= | --openssl_file= (Location of openssl.cnf. Default=./openssl.cnf)
 
 # Required
@@ -42,6 +33,7 @@ Usage: $0 (-i=<control_ip> | -d=<control_fqdn>) [-f=openssl_conf] -c=<cluster_na
 -h | --help (This help message)
 """
 
+IFS=","
 for arg in "$@"; do
   case $arg in
     -i=*|--control_ip=*)
@@ -64,6 +56,10 @@ for arg in "$@"; do
       NODES="${arg#*=}"
       shift # past argument with no value
       ;;
+    -k=*|--key_size=*)
+      KEY_SIZE="${arg#*=}"
+      shift
+      ;;
     -h|--help)
       echo -e $HELP_MSG && exit 0;
       shift # past argument with no value
@@ -73,15 +69,12 @@ for arg in "$@"; do
       ;;
   esac
 done
-# Config: The user must pass variables based on their cluster setup:
-# or receive defaults
+
 control_service_fqdn=${CONTROL_FQDN:=""}
 control_service_ip=${CONTROL_IP:=""}
+key_size=${KEY_SIZE:="4096"}
 
-# fill in the nodes to generate certs for the nodes
-# pass as a list of nodes
-# Example:  --nodes=node1,node2,node3...
-# maybe we want the ability to pass a json/yaml/csv to this?
+# Sanity checks
 if [ -z "$NODES" ] || [ -z "${NODES// }" ]; then
   echo "No nodes provided! Exiting!"
   echo "${HELP_MSG}"
@@ -94,13 +87,14 @@ if [ -z "$CLUSTER_NAME" ] || [ -z "${CLUSTER_NAME// }" ]; then
   exit 1
 fi
 
+# Split nodes into discrete values
 declare -a nodes
 for node in $NODES; do
   nodes+=( $node )
 done
 
 cluster_name=${CLUSTER_NAME}
-openssl_cnf_path=${OPENSSL_FILE:="./openssl.cnf"}
+openssl_cnf_path=${OPENSSL_FILE:="$CURRENT_DIR/openssl.cnf"}
 
 # set the CERT_HOST_ID environment variable early on since its used in
 # openssl.cnf. Use FQDN first if its there.
@@ -116,63 +110,94 @@ else
   exit 1
 fi
 
-# ----------------------------------------
-# generate the CA:
-# ----------------------------------------
+# Create needed CA fs layout
+CSR_DIR=$CURRENT_DIR/ssl/csr
+NEW_CERTS_DIR=$CURRENT_DIR/ssl/newcerts
+rm -rf $CURRENT_DIR/ssl
+mkdir -p $CSR_DIR
+mkdir -p $NEW_CERTS_DIR
+touch $(dirname $0)/ssl/index.txt
+echo 'unique_subject = no' > $(dirname $0)/ssl/index.txt.attr
+echo '1000' > ssl/serial
+
+echo "Generating the CA keypair"
 cluster_uuid=$(uuidgen)
 cluster_key_path=cluster.key
 cluster_crt_path=cluster.crt
 subject="/CN=$cluster_name/OU=$cluster_uuid"
-openssl genrsa -out $cluster_key_path 4096
-openssl req -batch -config $openssl_cnf_path -key $cluster_key_path -new -x509 -days 7300 -sha256 -extensions v3_ca -subj "$subject" -out $cluster_crt_path
+openssl genrsa -out $cluster_key_path $key_size
+openssl req -batch \
+            -config $openssl_cnf_path \
+            -key $cluster_key_path \
+            -new \
+            -x509 \
+            -days 7300 \
+            -sha256 \
+            -extensions v3_ca \
+            -subj "$subject" \
+            -out $cluster_crt_path
 
-# ----------------------------------------
-# generate the control cert and keypair
-# ----------------------------------------
+echo "Generating the control service keypair"
 # these end up getting copied to the nodes as control-service.(key|crt)
-# but we'll create them like the docs tell us to...
 control_key_path=control-$control_host.key
-control_csr_path=csr/control-$control_host.csr
+control_csr_path=$CURRENT_DIR/ssl/csr/control-$control_host.csr
 control_crt_path=control-$control_host.crt
 subject="/CN=control-service/OU=$cluster_uuid"
-# key
-openssl genrsa -out $control_key_path 4096
-# cert request
-#openssl req -config $openssl_cnf_path -key $control_key_path -new -days 7300 -sha256 -subj "$subject" -out $control_csr_path
-openssl req -config $openssl_cnf_path -key $control_key_path -new -days 7300 -sha256 -subj "$subject" -extensions control_service_extension -out $control_csr_path
-# cert
-#openssl ca -batch -config $openssl_cnf_path -keyfile $cluster_key_path -cert $cluster_crt_path -days 7300 -notext -md sha256 -extfile $host_extension_path -in $control_csr_path -subj "$subject" -out $control_crt_path
-openssl ca -batch -config $openssl_cnf_path -keyfile $cluster_key_path -cert $cluster_crt_path -days 7300 -notext -md sha256 -extensions control_service_extension -in $control_csr_path -subj "$subject" -out $control_crt_path
+openssl genrsa -out $control_key_path $key_size
+openssl req -config $openssl_cnf_path -key $control_key_path -new -days 7300 -sha256 -subj "$subject" -out $control_csr_path
+openssl ca -batch \
+           -config "$openssl_cnf_path" \
+           -keyfile "$cluster_key_path" \
+           -cert "$cluster_crt_path" \
+           -days 7300 \
+           -notext \
+           -md sha256 \
+           -extensions "control_service_extension" \
+           -in "$control_csr_path" \
+           -subj "$subject" \
+           -out "$control_crt_path"
 
-#----------------------------------------
-# Generate the node cert and keypair
-#----------------------------------------
-# you will need to do this for each node in your cluster
+echo "Generating node keypair(s)"
 for node_hostname in ${nodes[@]}; do
-    mkdir $node_hostname
+    mkdir -p $node_hostname
+
     node_uuid=$(uuidgen)
     node_key_path=$node_hostname/node-$node_uuid.key
-    node_csr_path=csr/node-$node_uuid.csr
+    node_csr_path=$CURRENT_DIR/ssl/csr/node-$node_uuid.csr
     node_crt_path=$node_hostname/node-$node_uuid.crt
-    # key
-    openssl genrsa -out $node_key_path 4096
-    # cert request
+
+    openssl genrsa -out $node_key_path $key_size
     subject="/CN=node-$node_uuid/OU=$cluster_uuid"
     openssl req -config $openssl_cnf_path -key $node_key_path -new -sha256 -subj "$subject" -out $node_csr_path
-    # cert
-    openssl ca -batch -config $openssl_cnf_path -keyfile $cluster_key_path -cert $cluster_crt_path -days 7300 -notext -md sha256 -in $node_csr_path -subj "$subject" -out $node_crt_path
+    openssl ca -batch \
+               -config "$openssl_cnf_path" \
+               -keyfile "$cluster_key_path" \
+               -cert "$cluster_crt_path" \
+               -days 7300 \
+               -notext \
+               -md sha256 \
+               -in "$node_csr_path" \
+               -subj "$subject" \
+               -out "$node_crt_path"
 done
 
-#generate api cert and keypair
-#----------------------------------------
+echo "Generating API keypair"
 api_username=api_user
 api_key_path=$api_username.key
-api_csr_path=csr/$api_username.csr
+api_csr_path=$CURRENT_DIR/ssl/csr/$api_username.csr
 api_crt_path=$api_username.crt
 subject="/CN=user-$api_username/OU=$cluster_uuid"
-# key
-openssl genrsa -out $api_key_path 4096
-# cert request
+openssl genrsa -out $api_key_path $key_size
 openssl req -config $openssl_cnf_path -key $api_key_path -new -sha256 -subj "$subject" -out $api_csr_path
-# cert
-openssl ca -batch -config $openssl_cnf_path -keyfile $cluster_key_path -cert $cluster_crt_path -days 7300 -notext -md sha256 -in $api_csr_path -subj "$subject" -extensions client_api_ext -out $api_crt_path
+openssl ca -batch \
+           -config "$openssl_cnf_path" \
+           -keyfile "$cluster_key_path" \
+           -cert "$cluster_crt_path" \
+           -days 7300 \
+           -notext \
+           -md sha256 \
+           -in "$api_csr_path" \
+           -subj "$subject" \
+           -extensions "client_api_ext" \
+           -out "$api_crt_path"
+echo "Done!"
